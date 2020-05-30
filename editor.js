dad46cae6f95192b40c5266c5826c2c8cf490b08
AgataOstrowska <56037254+AgataOstrowska@users.noreply.github.com>
2020-05-30 13:52:03 +0200

    ##6
    ///

--- editor.js
+++ editor.js
@@ -102,67 +102,218 @@ function Editor(settings) {
         }
     }
 
-
     let bold = document.getElementById("bold")
 
+    function unBold(selection) {
 
-    bold.addEventListener('click', (e) => {
-        let selection = getSelection()
+    }
 
-        let startEl = selection.startNode
-        let endEl = selection.endNode
+    
 
-        //nazewnictwo z selection
-
-        let rangeClone = {
-            startOffset: selection.startOffset,
-            endOffset: selection.endOffset,
-            startNode: startEl,
-            endNode: endEl,
-            startBlock: startEl.parentElement,
-            endBlock: endEl.parentElement,
-        };
-
-        if(startEl === endEl){
-            let str = startEl.textContent
-            let textStart = str.substring(0, range.startOffset)
-            let textEnd = str.substring(range.endOffset,startEl.textContent.length - 1)
-            let textToBold = str.substring(range.startOffset,range.endOffset)
-            startEl.innerHTML = 
-                `${textStart}<span style="font-weight: bold; color: black;">${textToBold}</span>${textEnd}`;
-            
-            rangeClone.startContainer = startEl.children[0].childNodes[0];
-            rangeClone.endContainer = startEl.children[0].childNodes[0];
-            rangeClone.endOffset = rangeClone.endOffset - rangeClone.startOffset
-        } else {
-            let offset = range.startOffset;
-            while(startEl != endEl) {
-                let str = startEl.textContent
-                let textStart = str.substring(0, offset);
-                let textToBold = str.substring(offset, str.length - 1);
-                startEl.innerHTML = 
-                    `${textStart}<span style="font-weight: bold; color: black;">${textToBold}</span>`;
-                if(offset != 0){
-                    rangeClone.startContainer = startEl.children[0].childNodes[0];
-                }
-                offset = 0
-                startEl = startEl.nextSibling
-            }
-            let str = endEl.textContent;
-            let textStart = str.substring(0, range.endOffset);
-            let textEnd = str.substring(range.endOffset, str.length - 1);
-            startEl.innerHTML = `<span style="font-weight: bold; color: black;">${textStart}</span>${textEnd}`;
-            rangeClone.endContainer = startEl.children[0].childNodes[0];
+     /*
+        node {
+            element : HTMLElemnt,
+            startOffset : ( startOffset : number ),
+            endOffset : ( endOffset : number ),
+            startNode : HTMLElement(span),
+            endNode : HTMLElement(span)
+            length : HTMLElemnt.textContent.length
+        }
+    */
+    function boldBlock(block){
+        let currNode = 
+            block.startNode == block.endNode 
+                ? block.startNode
+                : block.startNode.nextElementSibling 
+        
+        while(currNode != block.endNode) {
+            currNode.style.fontWeight = 'bold'
+            currNode = currNode.nextElementSibling;
         }
 
-        let newRange = new Range()
-        newRange.setStart(rangeClone.startContainer, 0)
-        newRange.setEnd(rangeClone.endContainer, rangeClone.endOffset);
-        
-        let nselection = window.getSelection()
-        nselection.removeAllRanges()
-        nselection.addRange(newRange)
+        if(block.startNode === block.endNode) {
+            return boldNode({
+              element: block.startNode,
+              startOffset: block.startOffset,
+              endOffset: block.endOffset,
+              length: block.startNode.textContent.length,
+            })
+        }else {
+            let startNode = boldNode({
+              element: block.startNode,
+              startOffset: block.startOffset,
+              endOffset: block.startNode.textContent.length,
+              length: block.startNode.textContent.length,
+            })
+            let endNode = boldNode({
+              element: block.endNode,
+              startOffset: 0,
+              endOffset: block.endOffset,
+              length: block.endNode.textContent.length,
+            });
 
+            return {
+              startNode: startNode.startNode,
+              endNode: endNode.endNode,
+              startOffset: startNode.startOffset,
+              endOffset: endNode.endOffset,
+            };
+        }
+    }
+    /*
+        node {
+            element : HTMLElemnt,
+            startOffset : ( startOffset : number ),
+            endOffset : ( endOffset : number ),
+            length : HTMLElemnt.textContent.length
+        }
+    */
+    function boldNode(node){
+        if (node.startOffset === 0 && node.endOffset === node.length) {
+          return (node.element.style.fontWeight = 'bold');
+        }
+        if (node.startOffset === 0) {
+          let bold = createNode(node.element.style);
+          bold.style.fontWeight = 'bold';
+
+          let resNode = createNode(node.element.style);
+          node.element.replaceWith(bold);
+
+          bold.textContent = node.element.textContent.substring(
+            node.startOffset,
+            node.endOffset
+          );
+
+          resNode.textContent = node.element.textContent.substring(
+            node.endOffset,
+            node.length
+          );
+
+          insertAfter(resNode, bold);
+
+          return {
+            startNode: bold,
+            endNode: bold,
+            startOffset: 0,
+            endOffset: bold.textContent.length,
+          }
+        }
+
+        if (node.endOffset === node.length) {
+          let bold = createNode(node.element.style);
+          bold.style.fontWeight = 'bold';
+
+          let resNode = createNode(node.element.style);
+          node.element.replaceWith(resNode);
+
+          bold.textContent = node.element.textContent.substring(
+            node.startOffset,
+            node.endOffset
+          );
+
+          resNode.textContent = node.element.textContent.substring(
+            0,
+            node.startOffset
+          );
+
+          insertAfter(bold, resNode);
+
+          return {
+            startNode: bold,
+            endNode: bold,
+            startOffset: 0,
+            endOffset: bold.textContent.length,
+          }
+        }
+
+        let bold = createNode(node.element.style);
+        bold.style.fontWeight = 'bold';
+
+        let resStartNode = createNode(node.element.style);
+        let resEndNode = createNode(node.element.style);
+
+        bold.textContent = node.element.textContent.substring(
+          node.startOffset,
+          node.endOffset
+        );
+
+        resStartNode.textContent = node.element.textContent.substring(
+          0,
+          node.startOffset
+        );
+
+        resEndNode.textContent = node.element.textContent.substring(
+          node.endOffset,
+          node.length
+        );
+
+        node.element.replaceWith(resStartNode);
+        insertAfter(bold, resStartNode);
+        insertAfter(resEndNode, bold);
+
+        return {
+          startNode: bold,
+          endNode: bold,
+          startOffset: 0,
+          endOffset: bold.textContent.length,
+        }
+    }
+
+//POPRAWIĆ UNBOLD
+    bold.addEventListener('click', (e) => {
+        let selection = getSelection()
+        
+        if(selection.startBlock === selection.endBlock){
+            setSelection(
+              boldBlock({
+                element: selection.startBlock,
+                startNode: selection.startNode,
+                endNode: selection.endNode,
+                startOffset: selection.startOffset,
+                endOffset: selection.endOffset,
+              })
+            )
+        }else {
+            let currBlock = 
+                selection.startBlock === selection.endBlock 
+                    ? selection.startBlock
+                    : selection.startBlock.nextElementSibling
+
+            while(selection.endBlock != currBlock) { 
+                boldBlock({
+                    element: currBlock,
+                    startNode: currBlock.firstElementChild,
+                    endNode: currBlock.lastElementChild,
+                    startOffset: 0,
+                    endOffset: currBlock.lastElementChild.textContent.length
+                })
+
+                currBlock = currBlock.nextElementSibling
+            }
+
+            let startSelection = boldBlock({
+              element: selection.startBlock,
+              startNode: selection.startNode,
+              endNode: selection.startBlock.lastElementChild,
+              startOffset: selection.startOffset,
+              endOffset: selection.startBlock.lastElementChild.textContent.length,
+            });
+
+            let endSelecition = boldBlock({
+              element: selection.endBlock,
+              startNode: selection.endBlock.firstElementChild,
+              endNode: selection.endNode,
+              startOffset: 0,
+              endOffset: selection.endOffset,
+            });
+
+            setSelection({
+              startNode: startSelection.startNode,
+              endNode: endSelecition.endNode,
+              startOffset: startSelection.startOffset,
+              endOffset: endSelecition.endOffset,
+            });
+        }
     })
 
 
@@ -173,7 +324,7 @@ function Editor(settings) {
     var isElement = (node) => 
         node.nodeType === Node.ELEMENT_NODE ?  true : false
     
-    var getSelection = () => {
+    var getSelection = () => { // <- to jest wazne!
         let selection = editor.ownerDocument.getSelection().getRangeAt(0)
 
         let startNode = getElementFromNode(selection.startContainer)
@@ -197,7 +348,6 @@ function Editor(settings) {
     var setSelection = (newSelection) => {
         let selection = editor.ownerDocument.getSelection()
         let range = document.createRange()
-
         range.setStart(
             newSelection.startNode.childNodes[0], 
             newSelection.startOffset
@@ -223,6 +373,62 @@ function Editor(settings) {
             node.dataset.type === 'text')  
                 ? true : false
 
+    var addLine = (selection) => { 
+        deleteFromSelection(selection);
+        
+        let node   = selection.startNode,
+            offset = selection.startOffset
+        
+        let blockSelection = node.parentElement
+
+        if(blockSelection.lastElementChild == node && offset == node.textContent.length) {
+            return addEmptyLine(selection)
+        }
+
+        let currNode = node.nextElementSibling
+        let nodeList = []
+
+        while(currNode != null) {
+            nodeList.push(currNode)
+            let next = currNode.nextElementSibling
+            blockSelection.removeChild(currNode)
+            currNode = next
+        }
+        
+        let block = document.createElement('p')
+        block.dataset.type = 'block'
+
+        let tempNode = document.createElement('span')
+        
+        for(let i = 0; i < node.style.length; i++)
+            tempNode.style[node.style.item(i)] = node.style[node.style.item(i)]
+
+        
+
+        tempNode.textContent = node.textContent.substring(
+          selection.startOffset,
+          node.textContent.length
+        );
+
+        node.textContent = node.textContent.substring(
+            0, selection.startOffset
+        )
+
+        block.innerHTML =
+          `${tempNode.textContent && tempNode.outerHTML }` +
+          nodeList.map((node) => node.outerHTML).join();
+
+        insertAfter(block, node.parentElement)
+        setSelection({
+            startBlock: block,
+            endBlock: block,
+            startNode: block.firstElementChild,
+            endNode: block.firstElementChild,
+            startOffset: 0,
+            endOffset: 0
+        })
+    }
+
 
     var addEmptyLine = (selection) => {
         let block = elementWithChilds('p', [
@@ -243,46 +449,96 @@ function Editor(settings) {
         })
     }
 
-
-    /* OD TEGO MIEJSCA ZACZAĆ */
-    /* !!!!! */
     var deleteFromSelection = (selection) => {
-          let currBlock =
-            selection.startBlock == selection.endBlock
-              ? selection.startBlock
-              : selection.startBlock.nextSibling;
-
-          while (currBlock != selection.endBlock) {
-            let next = currBlock.nextSibling;
-            editor.removeChild(currBlock);
-            currBlock = next;
-          }
-
-          let currNode = selection.startNode.nextSibling;
-
-          if (selection.startBlock != selection.endBlock) {
-            while (currNode != null) {
-              let next = currNode.nextSibling;
-              selection.startBlock.removeChild(currNode);
-              currNode = next;
+        if(!selection.isCollapsed) {
+            let setNode = true
+            let currBlock =
+                selection.startBlock == selection.endBlock
+                    ? selection.startBlock
+                    : selection.startBlock.nextElementSibling;
+            
+            while (currBlock != selection.endBlock) {
+                let next = currBlock.nextElementSibling;
+                editor.removeChild(currBlock);
+                currBlock = next;
             }
-          }
 
-          currNode = selection.endBlock.firstChild;
-          while (currNode != selection.endNode) {
-            let next = currNode.nextSibling;
-            selection.endBlock.removeChild(currNode);
-            currNode = next;
-          }
+            let currNode = 
+                selection.startNode == selection.endNode 
+                    ? selection.startNode 
+                    : selection.startNode.nextElementSibling 
+                
+            if (selection.startBlock == selection.endBlock ){
+                if(selection.startNode == selection.endNode){
+                    let str = selection.startNode.textContent.substring(
+                      0,
+                      selection.startOffset
+                    ); 
 
-          selection.startNode.textContent = selection.startNode.textContent.substring(
-            0,
-            selection.startOffset
-          );
-          selection.endNode.textContent = selection.endNode.textContent.substring(
-            selection.endOffset,
-            selection.endNode.textContent.length
-          );
+                    str += selection.startNode.textContent.substring(
+                        selection.endOffset,
+                        selection.startNode.textContent.length
+                    )
+                    setNode = false
+                    selection.startNode.textContent = str
+                    setSelection({
+                        startBlock: selection.endBlock,
+                        endBlock: selection.endBlock,
+                        startNode: selection.endNode,
+                        endNode: selection.endNode,
+                        startOffset: selection.startOffset,
+                        endOffset: selection.startOffset,
+                    });
+                }else {
+                    while (currNode != selection.endNode) {
+                        let next = currNode.nextElementSibling
+                        selection.startBlock.removeChild(currNode)
+                        currNode = next
+                    }
+                }
+            } else {
+                let startNode = selection.startNode
+                let endNode = selection.startBlock.lastElementChild
+
+                let currNode = startNode.nextElementSibling
+
+                while (currNode != null ){
+                    let next = currNode.nextElementSibling;
+                    selection.startBlock.removeChild(currNode);
+                    currNode = next;
+                }
+
+                startNode = selection.endBlock.firstElementChild;
+                endNode = selection.endNode
+
+                currNode = startNode
+
+                while (currNode != endNode) {
+                    let next = currNode.nextElementSibling;
+                    selection.endBlock.removeChild(currNode);
+                    currNode = next;
+                }
+            }
+
+            if(setNode) {
+                let startContent = selection.startNode.textContent
+                let endContent = selection.endNode.textContent
+
+                let startNode = selection.startNode
+                let endNode = selection.endNode
+                startNode.textContent = startContent.substring(0, selection.startOffset);
+                endNode.textContent = endContent.substring(selection.endOffset, endNode.textContent.length)
+
+                setSelection({
+                    startBlock: selection.endBlock,
+                    endBlock: selection.endBlock,
+                    startNode: selection.endNode,
+                    endNode: selection.endNode,
+                    startOffset: 0,
+                    endOffset: 0
+                })
+            }
+        }
     }
     /* End Utils */
 
@@ -301,28 +557,12 @@ function Editor(settings) {
 
             case 'Enter':
                 e.preventDefault()
-                let selection = getSelection()
-
-                if(!selection.isCollapsed)
-                    deleteFromSelection(selection)
-                else 
-                    setSelection({
-                        startNode: selection.startNode,
-                        endNode: selection.startNode,
-                        startOffset: selection.startNode.textContent.length,
-                        endOffset: selection.startNode.textContent.length,
-                    });
-
-                addEmptyLine(getSelection())
+                addLine(getSelection());
             default:
                 break;
         }
     })
 
-    editor.addEventListener('keyup', (e) => {
-        cursor.el = window.getSelection().getRangeAt(0).startContainer;
-        cursor.offset = window.getSelection().getRangeAt(0).startOffset;
-    })
 
     editor.addEventListener('focus', (e) => {
         if (editor.children[0] == placeholderTag) {
